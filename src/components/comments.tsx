cfb6e48e46dfabc452261036bb40532986bda9e5
Soyuj Jung Basnet <bsoyuj@gmail.com>
2021-03-03 00:47:00 +0545

    Revert to utterances comment label issue

--- src/components/comments.tsx
+++ src/components/comments.tsx
@@ -9,7 +9,7 @@ const Comment = ({ commentBox, theme }) => {
     scriptEl.src = 'https://utteranc.es/client.js';
     scriptEl.setAttribute('repo', 'basnetsoyuj/personal-website');
     scriptEl.setAttribute('issue-term', 'title');
-    scriptEl.setAttribute('label', 'comment');
+    scriptEl.setAttribute('label', 'comment 💬');
     scriptEl.setAttribute('id', 'utterances');
     scriptEl.setAttribute('theme', `github-${theme ? 'dark' : 'light'}`);
     scriptEl.setAttribute('crossorigin', 'anonymous');
